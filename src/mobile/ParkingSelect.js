8016d465aeff8a8ec519ec778ebdf56d45d1e63c
jujuicy97 <jujuicy97@gmail.com>
2025-07-24 17:59:35 +0900

    0724 18:00

---
+++ src/mobile/ParkingSelect.js
@@ -0,0 +1,10 @@
+
+const ParkingSelect = () => {
+  return (
+    <div>
+      <p>주차 구역 지정 페이지</p>
+    </div>
+  );
+};
+
+export default ParkingSelect;
--- src/pages/MobilePage.js
+++ src/pages/MobilePage.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import FloorSelect from "../mobile/FloorSelect";
+import ParkingSelect from "../mobile/ParkingSelect";
 
 const MobilePage = () => {
   return (
     <div className="mobile-page">
       <Routes>
         <Route path="/" element={<FloorSelect />}/>
+        <Route path="/parkingSelect" element={<ParkingSelect />}/>
       </Routes>
     </div>
   );

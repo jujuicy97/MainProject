ca8a9b0eadcddc0cc09101b5ba66cc7a961d99b8
jujuicy97 <jujuicy97@gmail.com>
2025-07-24 09:51:40 +0900

    0724 9:51

--- src/pages/MobilePage.js
+++ src/pages/MobilePage.js
@@ -1,11 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import StartPage from "../mobile/StartPageMobile";
 
 const MobilePage = () => {
   return (
     <div className="mobile-page">
       <Routes>
-        <Route path="/" element={<StartPage />}/>
+
       </Routes>
     </div>
   );

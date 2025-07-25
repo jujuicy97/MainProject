180cfe5cae9a992003a559b8410e9873cbb58045
jujuicy97 <jujuicy97@gmail.com>
2025-07-25 17:48:17 +0900

    0725 17:48

--- src/mobile/ParkingSelect.js
+++ src/mobile/ParkingSelect.js
@@ -4,15 +4,20 @@ import IconTitle from "./IconTitle";
 import { FaCarSide } from "react-icons/fa6";
 import { useState } from "react";
 
-const ParkingSelect = ({selectedZone, selectedZoneSeats}) => {
-  // console.log(selectedZone, selectedZoneSeats); //floorSelect에서의 정보가 잘 불러와짐!
+const ParkingSelect = ({ reservation }) => {
+  const {
+    selectedZone,
+    selectedZoneSeats,
+    selectSeatID,
+    setSelectSeatID    
+  } = reservation;
+  // console.log(selectedZone, selectedZoneSeats); //floorSelect에서 내가 선택한 구역의 정보가 잘 불러와짐!
 
   //seat 3개 구간으로 나누기(임시 배열 변수 생성)
   const oneRow = selectedZoneSeats.filter((seat)=>{return seat.num >= 1 && seat.num <= 10});
   const twoRow = selectedZoneSeats.filter((seat)=>{return seat.num >= 11 && seat.num <= 20});
   const threeRow = selectedZoneSeats.filter((seat)=>{return seat.num >= 21 && seat.num <= 25});
 
-  const [selectSeatID,setSelectSeatID] = useState(""); //현재 선택한 좌석 상태관리
   
   //예약이 가능한 seat만 선택이 되게 하는 함수(예약중, 예약불가능은 클릭 안됨)
   const handleSeatClick =(seat)=>{
@@ -33,7 +38,7 @@ const ParkingSelect = ({selectedZone, selectedZoneSeats}) => {
     })
   }
 
-
+// console.log(oneRow);
   return (
     <div className="parking-select">
       <div className="top-benner"></div>

eb0a8014c75751864948fddf7557e500449e367a
kimhyunseo <rlagustj14@gmail.com>
2025-07-28 17:29:50 +0900

    0728

--- src/mobile/BottomNavBarMobile.js
+++ src/mobile/BottomNavBarMobile.js
@@ -1,24 +1,39 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { TiHome } from "react-icons/ti";
 import { FaCar } from "react-icons/fa";
-import { HiTicket } from "react-icons/hi";
-import { HiInformationCircle } from "react-icons/hi";
+import { HiTicket, HiInformationCircle } from "react-icons/hi";
 
 const BottomNavBarMobile = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
   return (
     <nav id="bottom-navbar">
       <div className="navbar-container">
-        <div className="icon active" onClick={() => navigate('/')}>
+        <div
+          className={`icon ${isActive('/') ? 'active' : ''}`}
+          onClick={() => navigate('/')}
+        >
           <TiHome />
         </div>
-        <div className="icon" onClick={() => navigate('/')}>
+        <div
+          className={`icon ${isActive('/reservation/schedule') ? 'active' : ''}`}
+          onClick={() => navigate('/reservation/schedule')}
+        >
           <FaCar />
         </div>
-        <div className="icon" onClick={() => navigate('/')}>
+        <div
+          className={`icon ${isActive('/ticket') ? 'active' : ''}`}
+          onClick={() => navigate('/ticket')}
+        >
           <HiTicket />
         </div>
-        <div className="icon" onClick={() => navigate('/')}>
+        <div
+          className={`icon ${isActive('/info') ? 'active' : ''}`}
+          onClick={() => navigate('/info')}
+        >
           <HiInformationCircle />
         </div>
       </div>

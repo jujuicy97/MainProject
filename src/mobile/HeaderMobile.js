53296486ab4567eb5e4b47a8808cc3b714f527e3
hyeswe10 <jmoon981107@gmail.com>
2025-07-29 16:47:10 +0900

    0729 정우병합커밋

--- src/mobile/HeaderMobile.js
+++ src/mobile/HeaderMobile.js
@@ -1,20 +1,25 @@
-
 import { useNavigate } from "react-router-dom";
 import { FaCaretLeft } from "react-icons/fa";
 
-const HeaderMobile = () => {
+const HeaderMobile = ({ pageName }) => {
   const navigate = useNavigate();
+
+  const handleBack = () => {
+    if (window.history.length > 1) {
+      navigate(-1);  // 이전 페이지로 이동
+    } else {
+      navigate('/'); // 히스토리 없으면 홈으로 이동
+    }
+  };
+
   return (
     <nav id="top-navbar">
       <div className="navbar-container">
-        <div className="arrow-icon" onClick={() => navigate('/')}>   {/* 뒤로가기 네비게이터 설정 */}
+        <div className="arrow-icon" onClick={handleBack}>
           <FaCaretLeft />
         </div>
-        <p className="page-name">
-          예약하기
-        </p>
+        <p className="page-name">{pageName}</p>
       </div>
-
     </nav>
   );
 };
--- src/pages/MobilePage.js
+++ src/pages/MobilePage.js
@@ -1,36 +1,66 @@
 
 import MyPage from "../components/MyPage/MyPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import MainPageMobile from "../mobile/MainPageMobile";
 import MobileReservation from "../mobile/MobileReservation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HeaderMobile from "../mobile/HeaderMobile";
 import BottomNavBarMobile from "../mobile/BottomNavBarMobile";
+import Login from "../mobile/Login";
+import AgreeMent from "../mobile/AgreeMent";
+import SignUp from "../mobile/SignUp";
+import SignUpComplete from "../mobile/SignUpComplete";
+import FindID from "../mobile/FindID";
+import FindIDNo1 from "../mobile/FindIDNo1";
+import FindPW from "../mobile/FindPW";
+import ResetPw from "../mobile/ResetPw";
+import ChangedPw from "../mobile/ChangedPw";
 
 
 const MobilePage = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedZone, setSelectedZone] = useState(null);
-  const [selectedZoneSeats, setSelectedZoneSeats] = useState([]);
-  const [selectSeatID, setSelectSeatID] = useState(null);
-  //날짜, 구역, 좌석 상태 관리
-  const reservationState = {
-    selectedDate,
-    setSelectedDate,
-    selectedZone,
-    setSelectedZone,
-    selectedZoneSeats,
-    setSelectedZoneSeats,
-    selectSeatID,
-    setSelectSeatID    
-  };
-  //메인페이지 -> 예약페이지
+  const [userID,setUserID] = useState('');
+  const [id,setId] = useState(null);
+  const [nameOfPage,setNameOfPage] = useState('');
+  const location = useLocation();
+  useEffect(()=>{
+    const path = location.pathname;
+    if(path === '/login'){
+      setNameOfPage('로그인');
+    } else if(path === '/agreement' || path === '/signup' || path === '/signupComplete'){
+      setNameOfPage('회원가입')
+    } else if(path.includes("/findid")){
+      setNameOfPage('아이디 찾기')
+    } else if(path.includes('/findpw')){
+      setNameOfPage('비밀번호 변경')
+    } else if(path.includes("/mypage")){
+      if(path === '/mypage'){
+        setNameOfPage('마이페이지');
+      }
+      if(path.includes("/password-check")){
+        setNameOfPage('비밀번호변경');
+      }
+      if(path.includes("/membership")){
+        setNameOfPage('연간회원권 등록');
+      }
+    } else if(path.includes("/MobileReservation")){
+      setNameOfPage('예약하기')
+    }
+  },[location.pathname])
   return (
     <div className="mobile-page">
-      <MyPage />
-      <HeaderMobile />
+      <HeaderMobile pageName={nameOfPage}/>
       <Routes>
         <Route path="/" element={<MainPageMobile />}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/mypage/*" element={<MyPage/>}/>
+        <Route path="/agreement" element={<AgreeMent/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signupComplete" element={<SignUpComplete/>}/>
+        <Route path="/findid" element={<FindID setUserID={setUserID}/>}/>
+        <Route path="/findidno1" element={<FindIDNo1 userID={userID}/>}/>
+        <Route path="/findpw" element={<FindPW setID={setId}/>}/>
+        <Route path="/findpw/resetpw" element={<ResetPw ID={id}/>}/>
+        <Route path="/findpw/changedpw" element={<ChangedPw/>}/>
         <Route path="MobileReservation/*" element={<MobileReservation />}/>
       </Routes>
       <BottomNavBarMobile/>
